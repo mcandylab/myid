c33d00d4e44469e1a5ea2889b52ea36eb1b0a605
Andrey Abramov <an.abramov@internet.ru>
2024-12-07 23:38:51 +0300

    Сделал главную страницу

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -6,15 +6,15 @@ import { Button } from "@/components/ui/button.tsx";
 import { Badge } from "@/components/ui/badge.tsx";
 
 export const IndexPage: FC = () => {
-  const { user } = useSignal(initData.state);
+  const data = useSignal(initData.state);
   return (
     <Page back={false}>
       <div className="w-full leading-3 h-screen flex items-center justify-center flex-col gap-3 p-4">
         <p className="font-medium">
-          Привет {user.lastName} {user.firstName}!
+          Привет {data?.user?.lastName} {data?.user?.firstName}!
         </p>
-        <Badge>{user.username}</Badge>
-        <p className="text-sm">Ваш ID: {user.id}</p>
+        <Badge>{data?.user?.username}</Badge>
+        <p className="text-sm">Ваш ID: {data?.user?.id}</p>
         <Button>Shadcn UI Button</Button>
       </div>
     </Page>
